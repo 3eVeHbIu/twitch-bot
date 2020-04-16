88f151fe3d8e0be2d487c913f0fbde7426b2cc05
Sergey Arefiev <40056618+sergo2048@users.noreply.github.com>
2020-04-16 21:08:10 +0300

    set up simultaneous operation

--- main.py
+++ main.py
@@ -1,55 +1,45 @@
 # files
-import chat_comands
 import config
+import managers
 # libraries
 import asyncio
-import threading
-import socket
 import re
 
 
 CHAT_MESSAGE = re.compile(r'^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :')
+DEPARTURE_TEMPLATE = 'PRIVMSG #{} :{}\r\n'
 
 
 async def check_chat_messages():
     reader, writer = await asyncio.open_connection(config.HOST, config.PORT)
     writer.write('PASS {}\r\n'.format(config.PASSWORD).encode())
+    await writer.drain()
     writer.write('NICK {}\r\n'.format(config.NICK).encode())
+    await writer.drain()
     writer.write('JOIN #{}\r\n'.format(config.CHANNEL).encode())
     await writer.drain()
+    print('__Connected__')
+
     while True:
-        response = await reader.read(1024)
+        response = await reader.read(4096)
         response = response.decode()
         if response == 'PING :{}\r\n'.format(config.MESSAGE_INTERFACE):
             writer.write('PONG :{}\r\n'.format(
-                config.MESSAGE_INTERFACE).encode('utf-8'))
+                config.MESSAGE_INTERFACE).encode())
+            await writer.drain()
         else:
             username = re.search(r'\w+', response).group(0)
-            message = CHAT_MESSAGE.sub('', response)
-            print(username + ': ' + message)
-
-
-async def print_time():
-    count = 0
-    while True:
-        print('{} seconds have passed'.format(count))
-        count += 1
-        await asyncio.sleep(1)
+            message = CHAT_MESSAGE.sub('', response)[:-1]
+            if username in config.managers_list:
+                print(username + ': ' + message)
 
 
 async def async_distribution():
     tasks = []
+    tasks.append(asyncio.create_task(managers.fill_manager_list()))
     tasks.append(asyncio.create_task(check_chat_messages()))
-    tasks.append(asyncio.create_task(print_time()))
     await asyncio.gather(*tasks)
 
-
-
-
-
-    # filling_op_list = threading.Thread(target=sock_func.fill_op_list,
-    #                                    name='Thread2')
-    # filling_op_list.start()
 
 if __name__ == '__main__':
     asyncio.run(async_distribution())
--- managers.py
+++ managers.py
@@ -1,35 +1,34 @@
-from time import sleep
-import config
+from config import CHANNEL, managers_list
 from urllib.request import urlopen
 from json import loads
+import asyncio
+import aiohttp
 
 
-
-def fill_op_list():
+async def fill_manager_list():
     while True:
         try:
-            url = 'http://tmi.twitch.tv/group/user/{}/chatters'.format(
-                config.CHANNEL)
+            url = 'http://tmi.twitch.tv/group/user/{}/chatters'.format(CHANNEL)
             res = urlopen(url).read().decode()
             if res.find('502 bad getway') == -1:
-                config.oplist.clear()
+                managers_list.clear()
                 data = loads(res)
                 for item in data['chatters']['moderators']:
-                    config.oplist[item] = 'moderator'
+                    managers_list[item] = 'moderator'
                 for item in data['chatters']['staff']:
-                    config.oplist[item] = 'staff'
+                    managers_list[item] = 'staff'
                 for item in data['chatters']['admins']:
-                    config.oplist[item] = 'admin'
+                    managers_list[item] = 'admin'
                 for item in data['chatters']['global_mods']:
-                    config.oplist[item] = 'global_mod'
+                    managers_list[item] = 'global_mod'
             else:
                 print('ERROR 502: bad getway')
         except Exception as err:
             print('Faild with error:', str(err))
             break
-        print(config.oplist)
-        sleep(5)
+        await asyncio.sleep(5)
+        # после выхода из цикла
 
 
 def is_op(user):
-    return user in config.oplist
+    return user in managers_list
